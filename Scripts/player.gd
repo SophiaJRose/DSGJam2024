ab9fb38ba5e7386edb24c25dbbc746e17c94c14e
Sophia Jennifer Rose <sophia.j.rose@proton.me>
2024-02-23 22:02:10 +0000

    Initial commit, empty stage and basic movement

---
+++ Scripts/player.gd
@@ -0,0 +1,47 @@
+extends CharacterBody2D
+
+signal flash
+
+@export var walkSpeed = 384
+@export var jumpSpeed = 720
+@export var risingGrav = 32
+@export var fallingGrav = 64
+
+enum state {
+	STAND,
+	WALK,
+	JUMP
+}
+var playerState = state.STAND
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	velocity.x = 0
+	if is_on_floor():
+		playerState = state.STAND
+	if Input.is_action_pressed("move_right"):
+		velocity.x += walkSpeed
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= walkSpeed
+	if playerState == state.STAND and velocity.x != 0:
+		playerState = state.WALK
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y -= jumpSpeed
+		playerState = state.JUMP
+	if velocity.y <= 0 and !Input.is_action_pressed("jump"):
+		velocity.y /= 2
+	if velocity.y <= 0 and Input.is_action_pressed("jump"):
+		velocity.y += risingGrav
+	else:
+		velocity.y += fallingGrav
+		
+	if Input.is_action_just_pressed("flash"):
+		flash.emit()
+	
+	move_and_slide()
